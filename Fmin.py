f0a67e26cd1ae91bcc1bf16c67a438f6bf07bf60
jxhan <jxhan@Medivh.(none)>
2014-05-27 15:24:01 +0100

    added Fmin.py for min-dist estimator

---
+++ Fmin.py
@@ -0,0 +1,34 @@
+import sys,os
+from dynio import *
+os.environ['OMP_NUM_THREADS']='32'
+from iminuit import Minuit
+from iminuit.ConsoleFrontend import ConsoleFrontend
+
+estimator=8
+if len(sys.argv)>1:
+  estimator=int(sys.argv[1])
+
+get_config('AqA4')
+#os.environ['DynSIZE']='1000'
+os.environ['DynRMIN']='1'
+os.environ['DynRMAX']='200'
+init()
+select_particles(0)
+
+from scipy.optimize import *
+for estimator in [4,8,10,11]:
+  like= lambda x:-likefunc2(PARTYPE(x[0],x[1]), estimator)
+  #result=fmin(like, 10**(random.rand(2)-0.5), xtol=0.001, ftol=1e-4, maxiter=1000, maxfun=5000, full_output=True)
+  x=fmin(like, [1,1], xtol=0.001, ftol=1e-4)
+  print estimator, x
+  print '================================='
+ 
+def minuit_like(estimator=8):
+  neglike2=lambda m,c: -likefunc2(PARTYPE(m,c),estimator)
+  defaults={'m':1,'c':1}
+  m=Minuit(neglike2, print_level=3,errordef=0.5,limit_m=[0.1,10],limit_c=[0.1,10],frontend=ConsoleFrontend(),**defaults)
+  m.set_strategy(2);
+  m.tol=1e-3 #default convergence edm<1e-4*tol*errordef, but we do not need that high accuracy
+  result=m.migrad()
+  m.print_matrix()
+  return result,m
